c6d7776566f03074a2334b14d5f26c233a44daf2
Giovanni <gbaez89@gmail.com>
2024-12-02 09:26:24 -0600

    Solution to problem2 a

---
+++ P2/p2a_utils.jl
@@ -0,0 +1,43 @@
+module p2a_functions
+
+function load_data(file_name)
+    """A function to load data from a file into a list of arrays. This works if the number of element in each row is different."""
+    data = []
+    open(file_name, "r") do file
+        for line in eachline(file)
+            elements = split(line)
+            numbers = parse.(Int, elements)
+            push!(data, numbers)
+        end
+    end
+    return data
+end
+
+function cal_delta(data)
+    delta = [(data[i]-data[i+1]) for i in 1:(length(data)-1)]
+    return delta
+end
+
+function is_delta_safe(data)
+    #check if all elements are below 4 and above 0
+    safe = all([all(x-> 0 < abs(x) < 4, d) for d in data])
+    return safe
+end
+
+function monotic(data)
+    #check to see if all deltas are negative or postive
+    if all(x-> x > 0,data) || all(x-> x < 0,data) 
+        return true
+    else
+        return false 
+    end
+end
+
+function is_safe(data)
+    deltas = cal_delta.(data)
+    are_mono = monotic.(deltas)
+    safe_deltas = is_delta_safe.(deltas)
+    safe = are_mono .& safe_deltas #and combination of bool arrays
+    return sum(safe)
+end
+end
--- P2/run_solution_p2.jl
+++ P2/run_solution_p2.jl
@@ -1,12 +1,12 @@
-#include("p2a_utils.jl") #include the file containing all the functions for p1a
+include("p2a_utils.jl") #include the file containing all the functions for p2a
 #include("p2b_utils.jl") #include the file containing all the functions for p1b
-
-cd(@__DIR__) #make sure you are in the correct dir
+cd(@__DIR__)
+ #make sure you are in the correct dir
 #input files names
 #input_data = "test_input.txt" #testcase
 input_data = "input.txt" #real data
-
-data = input_data
-#solution goes here
+data = load_data(input_data)
+println("number of safe levels is:", is_safe(data))
+#output for testcase: number of safe levels is:2
 
 
