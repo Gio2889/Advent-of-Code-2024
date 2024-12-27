24a3548a363701a4eff0b5be1335680244a5e0b0
Giovanni <gbaez89@gmail.com>
2024-12-27 10:22:46 -0600

    tryingout astar algorithym

---
+++ P16/adjancency_matrix.jl
@@ -0,0 +1,57 @@
+# Input maze
+maze = [
+    "###############",
+    "#.......#....E#",
+    "#.#.###.#.###.#",
+    "#.....#.#...#.#",
+    "#.###.#####.#.#",
+    "#.#.#.......#.#",
+    "#.#.#####.###.#",
+    "#...........#.#",
+    "###.#.#####.#.#",
+    "#...#.....#.#.#",
+    "#.#.#.###.#.#.#",
+    "#.....#...#.#.#",
+    "#.###.#.#.#.#.#",
+    "#S..#.....#...#",
+    "###############"
+]
+
+# Parse the maze to create a mapping of traversable cells to indices
+rows = length(maze)
+cols = length(maze[1])
+index_map = Dict()  # Maps (row, col) -> node index
+current_index = 1
+
+# Populate the index_map
+for i in 1:rows, j in 1:cols
+    if maze[i][j] in ['.', 'S', 'E']
+        index_map[(i, j)] = current_index
+        global current_index += 1
+    end
+end
+
+# Number of nodes
+num_nodes = length(index_map)
+
+# Initialize the adjacency matrix
+adj_matrix = zeros(Int, num_nodes, num_nodes)
+
+# Function to check valid neighbors
+function get_neighbors(i, j)
+    return [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]  # Up, Down, Left, Right
+end
+
+# Build the adjacency matrix
+for (cell, idx) in index_map
+    i, j = cell
+    for neighbor in get_neighbors(i, j)
+        if haskey(index_map, neighbor)
+            neighbor_idx = index_map[neighbor]
+            adj_matrix[idx, neighbor_idx] = 1
+        end
+    end
+end
+
+# Print the adjacency matrix
+println(adj_matrix)
--- P16/p16a.jl
+++ P16/p16a.jl
@@ -6,125 +6,68 @@ function load_as_matrix(file_name)
     return char_matrix
 end
 
-# function maze_solver(maze_map)
-#     cols,rows = size(maze_map)
-#     score = 999999999999
-#     visited =[]
-#     function route(ci,cj,di,dj,current_score)
-#         if current_score > score #if the route overtakes the current score stop the loop
-#             return score #return the active score.
-#         end
-#         push!(visited,(ci,cj)) #mark spot as visited
-#         for (ei, ej) in [(-1, 0), (1, 0), (0, -1), (0, 1)]
-#             ni, nj = ci + ei, cj + ej
-#             if (maze_map[ni,nj] == '.' || maze_map[ni,nj] == 'E') &&  !((ni,nj) in visited)
-#                 if di == 0 && dj == 0 #direction not set
-#                     current_score +=1 # this move is always worth 1 point
-#                 else
-#                     if ei == dj && ej == dj ## direction is the same 
-#                         current_score += 1
-#                         if maze_map[ni,nj] == 'E'
-#                             return current_score # we have finished the maze
-#                         end
-#                         current_score =  route(ni,nj,ei,ej,current_score)  #move to the next step
-#                     else #direction is not the same, therefore we must turn 90 degrees since we cant go back
-#                         current_score += 1000
-#                         current_score = route(ni,nj,ei,ej,current_score)
-#                         if maze_map[ni,nj] == 'E'
-#                             return current_score # we have finished the maze
-#                         end
-#                     end              
-#                 end
-#                 return current_score
-#             else #if theres no possible neigbhor to move
-#                 return score #return the active score.
-#             end
-#         end
-#     end
-    
-    
-#     for i in 1:rows
-#         for j in 1:cols
-#             if maze_map[i,j] == 'S'
-#                 println("Starting maze solver")
-#                 score = route(i,j,0,0,score)
-#             end
-#         end
-#     end
-#     return score
-# end
-
-
-# input = "test_input.txt"
-# best_score = maze_solver(load_as_matrix(input))
 function direction_marker(ei, ej)
     return (ei == 1 && ej == 0 ? 'v' :   # Down
             ei == -1 && ej == 0 ? '^' :  # Up
             ei == 0 && ej == 1 ? '>' :   # Right
-            ei == 0 && ej == -1 ? '<' :  # Left
-            '.')                         # Default or invalid direction
+            ei == 0 && ej == -1 ? '<' :
+            '.')  # Left)                         # Default or invalid direction
 end
-
 
 function maze_solver(maze_map)
-    rows, cols = size(maze_map)
-    min_score = typemax(Int)  # Use a large integer instead of a hardcoded value
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
-
-    function route(ci, cj, di, dj, current_score, visited, local_map)
-        if maze_map[ci, cj] == 'E'  # Exit condition
-            return current_score, deepcopy(local_map)  # Return the score and the current map
+    cols,rows = size(maze_map)
+    score = typemax(Int)
+    visited =falses(rows,cols)
+    function route(ci,cj,di,dj,visited,current_score)
+        println("route at position $ci, $cj, with score $current_score")
+        if current_score > score #if the route overtakes the current score stop the loop
+            println("bad route; reseting")
+            return score,visited #return the active score.
         end
-    
-        local_score = current_score
-        best_map = deepcopy(local_map)  # To keep track of the best map
-        min_score = typemax(Int)  # Start with a large score
-        original_pos = local_map[ci,cj]
-        for (ei, ej) in directions
+        visited_local = copy(visited) #copy the visited array to not make global changes
+        visited_local[ci,cj] = true
+         #mark spot as visited
+        for (ei, ej) in [(-1, 0), (1, 0), (0, -1), (0, 1)]
             ni, nj = ci + ei, cj + ej
-    
-            if (maze_map[ni, nj] == '.' || maze_map[ni, nj] == 'E') && !(visited[ni, nj])
-                new_score = local_score + (ei != di || ej != dj ? 1000 : 1)  # Add turn cost or step cost
-                visited[ni, nj] = true
-
-                # Mark the map with the direction
-                local_map[ci, cj] = direction_marker(ei, ej)
-    
-                # Recursively explore
-                score, updated_map = route(ni, nj, ei, ej, new_score, visited, local_map)
-    
-                # Update min_score and best_map if this path is better
-                if score < min_score
-                    min_score = score
-                    best_map = deepcopy(updated_map)
+            if (maze_map[ni,nj] == '.' || maze_map[ni,nj] == 'E') &&  !(visited_local[ni,nj])
+                println("good neigbhor found")
+                if maze_map[ni,nj] == 'E'
+                    println("mazed finished")
+                    return current_score,visited_local # we have finished the maze
                 end
-            else #this no possible way out 
-                # Unmark visited cell and restore map
-                visited[ni, nj] = false
-                local_map[ci, cj] = original_pos  # Restore to empty path
+                if di == 0 && dj == 0 #direction not set
+                    # this move is always worth 1 point
+                    current_score,visited_local =  route(ni,nj,ei,ej,visited_local,current_score+1)
+                else
+                    println("same direction")
+                    if ei == di && ej == dj ## direction is the same 
+                        current_score,visited_local =  route(ni,nj,ei,ej,visited_local,current_score+1)  #move to the next step
+                    else #direction is not the same, therefore we must turn 90 degrees since we cant go back
+                        println("changin direction")
+                        current_score,visited_local = route(ni,nj,ei,ej,visited_local,current_score+1001)
+                    end              
+                end
+                return current_score,visited_local
+            # else #if theres no possible neigbhor to move
+            #     return score,visited_local #return the active score, previous visited map
             end
         end
-    
-        return min_score, best_map
     end
     
-    # Find the start position
+    
     for i in 1:rows
         for j in 1:cols
-            if maze_map[i, j] == 'S'
-                visited = falses(rows, cols)  # Boolean matrix to track visited positions
-                visited[i, j] = true
-                score, final_map = route(i, j, 0, 0, 0, visited, maze_map)
-                return score, final_map
+            if maze_map[i,j] == 'S'
+                println("Starting maze solver")
+                score = route(i,j,0,0,visited,0)
             end
         end
     end
-
-    return -1,maze_map  # Return -1 if no start position is found
+    return score
 end
 
-# Example usage
+
 input = "test_input.txt"
-best_score,visited_map = maze_solver(load_as_matrix(input))
-println("Best Score: ", best_score)
-display(visited_map)
+best_score,visited_final = maze_solver(load_as_matrix(input))
+display(visited_final)
+
